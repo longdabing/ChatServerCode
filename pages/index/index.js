ca9adc4bd8bd6a4f7d456053a6192857ef80d20a
longdb <576615843@qq.com>
2020-05-31 20:58:15 +0800

    新增images文件夹和底部导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,13 +13,21 @@ onLoad() {
     })
 },
 search: function (value) {
+    console.log(value,222);
     return new Promise((resolve, reject) => {
+        console.log(resolve,reject,33);
+        //resolve() 成功时调用，reject()失败时调用。
         setTimeout(() => {
             resolve([{text: '大饼', value: 1}, {text: '只说代码的大饼', value: 2}])
         }, 200)
     })
 },
 selectResult: function (e) {
-    console.log('select result', e.detail)
+    console.log('select result', e.detail.item)
+wx.showModal({
+  title:"搜索组件",
+  content:e.detail.item.text
+
+})
 },
 })
